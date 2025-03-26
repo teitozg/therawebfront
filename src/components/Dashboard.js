dfe4bb6886c8fdb0c874b8ee756a9742617b6a6c
Teo Zavalia Gahan <teitozg@Teos-MacBook-Pro.local>
2025-03-26 02:18:20 -0400

    Fix: Adjust scaling and padding for dashboard and transactions table

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,13 +5,20 @@ function Dashboard() {
   return (
     <div className="page-container">
       <h2>Reconciliation Dashboard</h2>
-      <div className="dashboard-container">
+      <div className="dashboard-container" style={{ padding: "0 30px" }}>
         <iframe
           width="100%"
           height="800"
           src="https://lookerstudio.google.com/embed/reporting/bb1feddc-579f-4f11-aadd-cc966b227720/page/bR1AF"
           frameBorder="0"
-          style={{ border: 0, borderRadius: "8px", padding: "20px" }}
+          style={{
+            border: 0,
+            borderRadius: "8px",
+            padding: "20px",
+            transform: "scale(0.9)",
+            transformOrigin: "top left",
+            width: "111%",
+          }}
           allowFullScreen
           sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
         />
